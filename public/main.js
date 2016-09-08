79a52fb20cf0a963b911cf6722edea7d1269eff9
Victor <Kamyshin.Victor@yandex.ru>
2016-09-08 18:23:22 +0300

    first home work

--- public/main.js
+++ public/main.js
@@ -4,11 +4,79 @@ function onSubmit(form) {
         user: form.elements['user'].value,
         email: form.elements['email'].value
     };
-    let result = request('/users', data);
-
-    if(result =='100') {
-         form.hidden = true;
-    }
-    request('/users', data);
+    let result = JSON.parse(request('/users', data));
+    let visits = result.visits; 
+   // form.hidden = true;
+    helloWorld.innerHTML = hello(data.user, visits);
     console.log(data);
+    console.log(result);
 }
+
+function plural (number) {
+    switch(number) {
+        case 0:
+            return 'Здравствуй, дух';
+            break;
+        case 1:
+            return 'Рады приветствовать на нашем курсе!';
+            break;
+        case 2:
+            return 'Кликай дальше!! Еще осталось 13 раз(а)';
+            break;
+        case 13:
+            return 'Кликай дальше!! Еще осталось 2 раз(а)';
+            break;
+        case 15:
+            return '01001000 01101001 00101100 00100000 01100010 01110010 01101111';
+            break;
+        case 100:
+            return '01001000 01101001 00101100 00100000 01100010 01110010 01101111';
+            break;
+    }
+}
+
+function hello (text, visits) {
+    if ( isCyrillic(text) ) {
+	if ( visits != undefined ) {
+            return 'Привет, ' + text + '. Вы посетили страницу ' + visits + ' '+ my_plural(visits);
+	} else {
+	    return 'Привет, ' + text;
+	}
+    } else {
+        if ( visits != undefined ) {
+            return 'Hello, ' + text + ' . You visited this page ' + visits + ' ' +my_plural_eng(visits);
+        } else {
+            return 'Hello, ' + text;
+        }
+    }
+}
+
+function my_plural ( number ) {
+    if (number % 10  > 1 && number % 10 < 5 && (number > 20 || number < 10) ) {
+        return 'раза.';
+    } else {
+        return 'раз.';
+    }
+}
+
+function my_plural_eng ( number ) {
+    if (number > 1) {
+        return 'times.';
+    } else {
+        return 'time.';
+    }
+}
+
+var isCyrillic = function (my_text) {
+     return /[а-я]/i.test(my_text);
+}
+
+if (typeof exports ==='object') {
+    exports.hello = hello; 
+}
+
+if (typeof exports == 'object') {
+    exports.plural = plural;
+}
+
+
--- server.js
+++ server.js
@@ -8,9 +8,24 @@ app.use('/', express.static('public'));
 app.use(parser.json());
 app.use(technologger);
 
+var array = {};
+
 app.post('/users', (req, res) => {
     console.log(req.body);
-    res.send('100');
+    var my_response = {
+        status : '100',
+        visits : 0
+    };
+    if(req.body.email in array) {
+        array[req.body.email]+=1;
+        my_response.visits = array[req.body.email];
+        res.send( my_response );
+    } else {
+        array[req.body.email] = 1;
+        my_response.visits = array[req.body.email];
+        my_response.status = '200';
+        res.send( my_response );
+    }
     // TODO: вернуть количество обращений
     // Сколько пользователей с таким э-мейлом уже обращалось
     //https://learn.javascript.ru/object
